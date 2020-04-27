784030468749f8e8223a89feffbad23157d64852
milesial <milesial@users.noreply.github.com>
2020-04-27 10:01:07 -0700

    More editor options and vim mode

--- client/src/components/EditPanelOptions.vue
+++ client/src/components/EditPanelOptions.vue
@@ -31,10 +31,37 @@
         <v-list-item dense>
           <v-switch
             color="primary"
+            prepend-icon="mdi-keyboard"
+            v-model="options.vim"
+            label="VIM mode"
+          ></v-switch>
+        </v-list-item>
+        <v-list-item dense>
+          <v-switch
+            color="primary"
             prepend-icon="mdi-format-list-numbered"
             v-model="options.showLines"
             label="Show line numbers"
           ></v-switch>
+        </v-list-item>
+        <v-list-item dense>
+          <v-switch
+            color="primary"
+            prepend-icon="mdi-map"
+            v-model="options.minimap"
+            label="Show the minimap"
+          ></v-switch>
+        </v-list-item>
+        <v-list-item dense>
+          <v-slider
+            v-model="options.fontSize"
+            prepend-icon="mdi-format-size"
+            min="6"
+            max="50"
+            thumb-label
+            label="Font size"
+            inverse-label
+          ></v-slider>
         </v-list-item>
       </v-list>
   </v-card>
@@ -44,6 +71,7 @@
 <script>
 
 import * as monaco from 'monaco-editor'
+import { initVimMode } from 'monaco-vim';
 let themes = ['vs', 'vs-dark', 'hc-black']
 
 export default {
@@ -53,7 +81,10 @@ export default {
     options: {
       theme: 'vs',
       showLines: true,
-    }
+      vim: false,
+      fontSize: 16
+    },
+    vimMode: null,
   }),
   watch: {
     'options.theme': function(v) {
@@ -61,6 +92,19 @@ export default {
     },
     'options.showLines': function(v) {
       this.editor.updateOptions({ lineNumbers: v ? "on" : "off" })
+    },
+    'options.fontSize': function(v) {
+      this.editor.updateOptions({ fontSize: v })
+    },
+    'options.minimap': function(v) {
+      this.editor.updateOptions({ minimap: {enabled: v} })
+    },
+    'options.vim': function(v) {
+      if (v) {
+        this.data.vimMode = initVimMode(this.editor, null)
+      } else if (this.data.vimMode) {
+        this.data.vimMode.dispose()
+      }
     }
   }
 }
