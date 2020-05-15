5e1a825a479154009f5b5b4fe50a71e8080721a3
milesial <milesial@users.noreply.github.com>
2020-05-15 07:18:00 -0700

    Logger for server

--- server/configure.js
+++ server/configure.js
@@ -1,13 +1,16 @@
 const api = require('./api')
+const logger = require('./logger')
+const options = require('./options')
+const cors = require('cors')
 
 module.exports = app => {
+  logger.info('Starting backend and exposing API')
   app.disable('x-powered-by')
-  app.use((req, res, next) => {
-    // TODO: do not accept all
-    res.set('Access-Control-Allow-Origin', '*')
-    res.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
-    res.set('Access-Control-Allow-Methods', 'POST, GET')
-    next()
-  })
+  app.use(cors({
+    origin: options.FRONT_URL,
+    credentials: true,
+    preflightContinue: false
+  }))
+  app.options('*', cors())
   app.use('/', api)
 }
---
+++ server/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston')
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.simple(),
+      winston.format.colorize()
+    )
+  }))
+}
+
+module.exports = logger
